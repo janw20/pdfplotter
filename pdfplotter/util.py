6ec48d66489d464dd979654bb16f55825bb29e4c
Jan Wissmann <jan.wissmann.jw@gmail.com>
2025-04-23 14:19:47 +0200

    Add update_kwargs function

--- pdfplotter/util.py
+++ pdfplotter/util.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Any
+
 import numpy as np
 import sympy as sp
 
@@ -152,3 +154,47 @@ def to_str(
         )
 
     return result_str
+
+
+def update_kwargs(
+    kwargs: dict[str, Any],
+    kwargs_user: dict[str, Any] | list[dict[str, Any] | None] | None,
+    i: int | None = None,
+) -> dict[str, Any]:
+    """Update `kwargs` with `kwargs_user`. If `kwargs_user` is a list, `i` must be given to select the correct element.
+
+    Parameters
+    ----------
+    kwargs : dict[str, Any]
+        The keyword arguments to update.
+    kwargs_user : dict[str, Any] | list[dict[str, Any]  |  None] | None
+        The keyword arguments to update with.
+    i : int | None, optional
+        Index of `kwargs_user` to use if it is a list, by default None
+
+    Returns
+    -------
+    dict[str, Any]
+        The updated keyword arguments
+
+    Raises
+    ------
+    ValueError
+        If `kwargs_user` is a list and `i` is not given
+    """
+    if isinstance(kwargs_user, dict):
+        return kwargs | kwargs_user
+    elif isinstance(kwargs_user, list):
+        if i is not None:
+            if i < len(kwargs_user) and kwargs_user[i] is not None:
+                return (
+                    kwargs | kwargs_user[i]
+                )  # pyright: ignore[reportOperatorIssue]  # this is correct, but pyright cannot narrow the types of list elements
+            else:
+                return kwargs
+        else:
+            raise ValueError("i must be given if kwargs_user is a list")
+    elif kwargs_user is None:
+        return kwargs
+    else:
+        raise ValueError("kwargs_user must be a dict or a list")
